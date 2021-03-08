76c20eebacbf68c14b9eee4e76557a8d5aab5903
hyunsik Oh <40059866+guppy-bluegrass@users.noreply.github.com>
2021-03-08 20:18:49 +0900

    Update 스타트링크 타워.cpp

--- 11주차/오현식/스타트링크 타워.cpp
+++ 11주차/오현식/스타트링크 타워.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 using namespace std;
@@ -6,8 +7,6 @@ using namespace std;
 int N;
 int bitOfNum[10]={0b111101101101111,0b001001001001001,0b111001111100111,0b111001111001111,0b101101111001001,0b111100111001111,0b111100111101111,0b111001001001001,0b111101111101111,0b111101111001111};   // bit of 0~9
 char board[6][36];  //board[i][j] i:1~5 j:1~35
-int numOfCases[10]; // idx:1~9
-int numOfAllCases=1;
 double ans;
 
 int getbitOfNthNum(int n){
@@ -32,29 +31,21 @@ int main(int argc, const char * argv[]) {
     for(int i=1;i<=N;i++){
         int bitOfNthNum = getbitOfNthNum(i);
         bool isPossible=false;
+        vector<int> candidate;
+        double sumOfCandidate=0;
         for(int j=0;j<10;j++){
             if((bitOfNthNum|bitOfNum[j]) == bitOfNum[j]) {
                 isPossible =true;
-                numOfCases[i]++;
+                candidate.push_back(j);
             }
         }
+        for(int j=0;j<candidate.size();j++) sumOfCandidate+=candidate[j]*pow(10,N-i);
+        if(candidate.size()!=0) ans += sumOfCandidate/candidate.size();
         if(!isPossible){
             cout<<-1;
             return 0;
         }
     }
-    for(int i=1;i<10;i++)  {
-        if(numOfCases[i]) {
-            numOfAllCases *= numOfCases[i];
-        }
-    }
-    for(int i=1;i<=N;i++){
-        int bitOfNthNum = getbitOfNthNum(i);
-        for(int j=0;j<10;j++){
-            if((bitOfNthNum|bitOfNum[j]) == bitOfNum[j])    ans += j*pow(10,N-i)*numOfAllCases/numOfCases[i];
-        }
-    }
-    ans /= numOfAllCases;
     printf("%lf",ans);
     return 0;
 }
